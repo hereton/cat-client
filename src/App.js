39794934b586d9f3572ae0f1c9a95f12a01a8721
hereton1 <totsapon.m@ku.th>
2020-05-31 01:06:31 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -16,28 +16,125 @@ function App() {
   const [catList, setCatList] = useState(getInitCatList());
   const [isLoading, setIsLoading] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
-  const [selectedCat, setSelectedCat] = useState();
+  const [selectedCatData, setSelectedCatData] = useState();
+  const [currentSelectedCatData, setCurrentSelectedCatData] = useState();
+  const [errorText, setErrorText] = useState("");
+  const [image, setImage] = useState();
 
   function handleSelectedCat(selectedCat) {
-    setSelectedCat(selectedCat);
+    setSelectedCatData(selectedCat);
+    setCurrentSelectedCatData(selectedCat);
     setIsEditMode(true);
     console.log(selectedCat);
   }
-  async function handleSubmit(value) {
-    if (
-      value.name !== undefined &&
-      value.price !== undefined &&
-      value.imgUrl !== undefined
-    ) {
+  async function handleDeleteSelectedCat(id) {
+    console.log(id);
+    setIsLoading(true);
+
+    const result = await axios.delete("http://52.23.233.214:3000/item/" + id);
+    setIsLoading(false);
+    if (result.status === 200) {
+      const result = await axios.get("http://52.23.233.214:3000/item");
+      console.log(result.data.data);
+      if (result.data.data) {
+        setCatList(result.data.data);
+        setIsLoading(false);
+      }
+    }
+  }
+
+  async function handleInputFile(e) {
+    console.log("sent to server");
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+      console.log(e.target.files[0]);
+      // const result = await axios.post("http://52.23.233.214:3000/upload", {
+      //   file: imageFile,
+      // });
+      // console.log(result);
+    }
+  }
+
+  async function handleEditSubmit(value, image) {
+    console.log(value);
+    console.log(image);
+    const formData = new FormData();
+    formData.append("image", image);
+
+    const responed = await axios.post(
+      "http://52.23.233.214:3000/upload",
+      formData
+    );
+
+    console.log("responed from upload ", responed.data);
+    setIsLoading(true);
+    if (value.name !== undefined && value.price !== undefined) {
       console.log("post data", { ...value });
+      const params = {
+        name: value.name,
+        price: value.price,
+        imgUrl: responed.data.imgUrl,
+      };
       setIsLoading(true);
-      const result = await axios.post("http://52.23.233.214:3000/item/", {
-        ...value,
-      });
-      setIsLoading(false);
-      console.log(result);
+      const result = await axios.put(
+        "http://52.23.233.214:3000/item/" + value.id,
+        params
+      );
+      setErrorText("");
+      console.log(result.status === 200);
+      if (result.status === 200) {
+        const result = await axios.get("http://52.23.233.214:3000/item");
+        console.log(result.data.data);
+        if (result.data.data) {
+          setIsLoading(false);
+          setCatList(result.data.data);
+          setImage(null);
+        }
+      }
 
       console.log("complete upload to server.");
+    } else {
+      setErrorText("Please complete input");
+    }
+  }
+  async function handleSubmit(value, image) {
+    const formData = new FormData();
+    formData.append("image", image);
+    setIsLoading(true);
+
+    const responed = await axios.post(
+      "http://52.23.233.214:3000/upload",
+      formData
+    );
+    console.log("responed from upload ", responed.data);
+
+    if (value.name !== undefined && value.price !== undefined) {
+      const params = {
+        name: value.name,
+        price: value.price,
+        imgUrl: responed.data.imgUrl,
+      };
+      console.log(params);
+      const result = await axios.post(
+        "http://52.23.233.214:3000/item/",
+        params
+      );
+      setErrorText("");
+      console.log(result);
+      console.log(result.status === 200);
+      if (result.status === 200) {
+        const result = await axios.get("http://52.23.233.214:3000/item");
+        console.log(result.data.data);
+        if (result.data.data) {
+          setIsLoading(false);
+          setCatList(result.data.data);
+          setImage(null);
+        }
+      }
+
+      console.log("complete upload to server.");
+    } else {
+      setErrorText("Please complete input");
     }
   }
 
@@ -65,6 +162,7 @@ function App() {
           {catList.map((cat, index) => {
             return (
               <Box
+                key={cat.id + index.toString()}
                 margin="small"
                 onClick={() => {
                   handleSelectedCat(cat);
@@ -88,51 +186,99 @@ function App() {
           })}
         </Box>
       ) : (
-        <Box> loading </Box>
+        <Box>
+          <Box align="center"> Loading </Box>{" "}
+        </Box>
       )}
+      <Box>{errorText.length > 0 ? <Box>{errorText}</Box> : <></>}</Box>
       {isEditMode ? (
         <Box align="center">
-          <Form onSubmit={({ value }) => handleEditSubmit(value)}>
+          <Text weight="bold" margin={{ top: "medium" }}>
+            Edit selected cat !!
+          </Text>
+
+          <Form
+            value={currentSelectedCatData}
+            onChange={(nextValue) => setCurrentSelectedCatData(nextValue)}
+            onReset={() => setCurrentSelectedCatData(selectedCatData)}
+            onSubmit={({ value }) => handleEditSubmit(value, image)}
+          >
             <FormField name="id" htmlfor="textinput-id" label="ID">
-              <TextInput
+              <TextInput id="textinput-id" name="id" disabled={true} />
+            </FormField>
+            <FormField name="name" htmlfor="textinput-id" label="Name" required>
+              <TextInput id="textinput-id" name="name" />
+            </FormField>
+            <FormField
+              name="price"
+              htmlfor="textinput-id"
+              label="Price"
+              required
+            >
+              <TextInput id="textinput-id" name="price" required />
+            </FormField>
+            <FormField
+              name="imgUrl"
+              htmlfor="textinput-id"
+              label="Upload new image"
+            >
+              <input
+                type="file"
                 id="textinput-id"
-                name="id"
-                value={selectedCat.id}
-                disabled={true}
+                name="imgUrl"
+                accept="image/*"
+                onChange={(e) => handleInputFile(e)}
+                required
               />
             </FormField>
-            <FormField name="id" htmlfor="textinput-id" label="Name">
-              <TextInput id="textinput-id" name="id" />
-            </FormField>
-            <FormField name="price" htmlfor="textinput-id" label="Price">
-              <TextInput id="textinput-id" name="price" />
-            </FormField>
-            <FormField name="imgUrl" htmlfor="textinput-id" label="Link">
-              <TextInput id="textinput-id" name="imgUrl" />
-            </FormField>
             <Box direction="row" gap="medium">
-              <Button type="submit" primary label="Submit" />
+              <Button type="submit" primary label="Edit Cat" />
               <Button type="reset" label="Reset" />
-              <Button label="Delete" />
+              <Button
+                label="Delete"
+                onClick={() => handleDeleteSelectedCat(selectedCatData.id)}
+              />
               <Button label="Cancel" onClick={() => setIsEditMode(false)} />
             </Box>
           </Form>
         </Box>
       ) : (
         <Box align="center">
-          <Form onSubmit={({ value }) => handleSubmit(value)}>
-            <FormField name="name" htmlfor="textinput-id" label="Name">
+          <Text weight="bold" margin={{ top: "medium" }}>
+            Add more cats !!
+          </Text>
+
+          <Form onSubmit={({ value }) => handleSubmit(value, image)}>
+            <FormField name="name" htmlfor="textinput-id" label="Name" required>
               <TextInput id="textinput-id" name="name" />
             </FormField>
-            <FormField name="price" htmlfor="textinput-id" label="Price">
+            <FormField
+              name="price"
+              htmlfor="textinput-id"
+              label="Price"
+              required
+            >
               <TextInput id="textinput-id" name="price" />
             </FormField>
-            <FormField name="imgUrl" htmlfor="textinput-id" label="Link">
-              <TextInput id="textinput-id" name="imgUrl" />
+            <FormField name="imgUrl" htmlfor="textinput-id" label="Image">
+              <input
+                type="file"
+                id="textinput-id"
+                name="imgUrl"
+                accept="image/*"
+                onChange={(e) => handleInputFile(e)}
+                required
+              />
             </FormField>
             <Box direction="row" gap="medium">
               <Button type="submit" primary label="Submit" />
-              <Button type="reset" label="Reset" />
+              <Button
+                type="reset"
+                label="Reset"
+                onClick={() => {
+                  setErrorText("");
+                }}
+              />
             </Box>
           </Form>
         </Box>
@@ -142,10 +288,6 @@ function App() {
 }
 
 export default App;
-
-async function handleEditSubmit(value) {
-  console.log(value);
-}
 
 function getInitCatList() {
   return [
@@ -160,22 +302,12 @@ function getInitCatList() {
       imgUrl: "https://thestandard.co/wp-content/uploads/2020/02/25-1.jpg",
     },
     {
-      name: "Prayus",
-      price: 200,
-      imgUrl: "https://thestandard.co/wp-content/uploads/2020/02/25-1.jpg",
-    },
-    {
-      name: "varit",
-      price: 200,
-      imgUrl: "https://thestandard.co/wp-content/uploads/2020/02/25-1.jpg",
-    },
-    {
       name: "Totsapon",
       price: 200,
       imgUrl: "https://thestandard.co/wp-content/uploads/2020/02/25-1.jpg",
     },
     {
-      name: "Prayus",
+      name: "Totsapon",
       price: 200,
       imgUrl: "https://thestandard.co/wp-content/uploads/2020/02/25-1.jpg",
     },
